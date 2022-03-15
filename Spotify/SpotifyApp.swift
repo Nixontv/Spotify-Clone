023a2c4418f49fa5e48e31b221fe164665a8c806
Nikita Velichko <velichkonikita@gmail.com>
2022-03-15 22:20:00 +0100

    commit

---
+++ Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by Nikita Velichko on 31.12.20.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TabBar()
+        }
+    }
+}
